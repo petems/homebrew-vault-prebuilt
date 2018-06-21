68ca1aedcc180ae6c08c0a831b1f3d4643c28a1e
Peter Souter <petems@users.noreply.github.com>
2018-06-21 23:50:06 +0100

    Update vault.rb

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -3,7 +3,7 @@ class Vault < Formula
   installer that builds from released builds from https://www.vaultproject.io/downloads.html)"
   homepage 'https://vaultproject.io/'
   url 'https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_darwin_amd64.zip'
-  sha256 '7d3675539f54c5c4b16c68eeb754183dcba9bc6f48ed5ecb74254693793a825b'
+  sha256 'c092bd4d279494daf801eee1905d693ec505fd274e0587c69a64354df66af426'
 
   def install
     bin.install 'vault'
