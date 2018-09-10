84c4c5093a985a65200c3f2c561006594d59807b
petems <p.morsou+GIT@gmail.com>
2018-09-10 14:04:21 +0100

    Bump version to 0.11.1

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -2,8 +2,8 @@ class Vault < Formula
   desc "VaultSecures, stores, and tightly controls access to secrets. (Note: This is a custom
   installer that builds from released builds from https://www.vaultproject.io/downloads.html)"
   homepage 'https://vaultproject.io/'
-  url 'https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_darwin_amd64.zip'
-  sha256 'c092bd4d279494daf801eee1905d693ec505fd274e0587c69a64354df66af426'
+  url 'https://releases.hashicorp.com/vault/0.11.1/vault_0.11.1_darwin_amd64.zip'
+  sha256 '88c96123bb476d9a5c6a4517fd122872232eb6aef94ccdfea065bb8e2c9d0bd7'
 
   def install
     bin.install 'vault'
