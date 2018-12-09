2c7d43490c3b02d8a976a9761ed54bf16be85cfe
petems <p.morsou+GIT@gmail.com>
2018-12-09 21:22:18 +0000

    brew audit fixes

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,15 +1,21 @@
 class Vault < Formula
+  VAULT_VERSION = "0.11.5".freeze
 
-  VAULT_VERSION = '0.11.5'
-
-  desc "VaultSecures, stores, and tightly controls access to secrets. (Note: This is a custom
-  installer that builds from released builds from https://www.vaultproject.io/downloads.html)"
-  homepage 'https://vaultproject.io/'
+  desc "Installs Vault from pre-built binaries"
+  homepage "https://vaultproject.io/downloads.html"
   url "https://releases.hashicorp.com/vault/#{VAULT_VERSION}/vault_#{VAULT_VERSION}_darwin_amd64.zip"
-  sha256 '58630a89ed1657814fcd1e00226db83479e10b0019b118d125a78b02a7d15ac5'
   version VAULT_VERSION
+  sha256 "58630a89ed1657814fcd1e00226db83479e10b0019b118d125a78b02a7d15ac5"
 
   def install
     bin.install 'vault'
+  end
+
+  test do
+    pid = fork { exec bin/"vault", "server", "-dev" }
+    sleep 1
+    ENV.append "VAULT_ADDR", "http://127.0.0.1:8200"
+    system bin/"vault", "status"
+    Process.kill("TERM", pid)
   end
 end
